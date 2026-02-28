2482b540390b6e811250c7a363e3f4f92d66a076
Demonio <s20.rex9000@gmail.com>
2026-02-28 19:54:32 +0300

    Readed data from db

--- Models/SiteStats.cs
+++ Models/SiteStats.cs
@@ -6,10 +6,15 @@ namespace WebApplication1.Models
     [Table("site_stats")]
     public class SiteStats
     {
+        [Column("id")]
         public int id { get; set; }
+        [Column("total_clients")]
         public int TotalClients { get; set; }
-        public int TotalCars { get; set; }        
+        [Column("total_cars")]
+        public int TotalCars { get; set; }
+        [Column("company_start_date")]
         public int CompanyStartDate { get; set; }
+        [Column("client_bonus")]
         public int ClientBonus { get; set; }
 
     }
