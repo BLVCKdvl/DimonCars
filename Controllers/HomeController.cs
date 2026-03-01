7b47431a97faf14df9023cc366378fced1fed76b
Demonio <s20.rex9000@gmail.com>
2026-03-01 23:38:04 +0300

    created backend status page deleted deprecated code build clean architectire

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,54 +1,65 @@
+//using Microsoft.AspNetCore.Mvc;
+//using System.Diagnostics;
+//using WebApplication1.Models;
+//using WebApplication1.Data;
+//using WebApplication1.Services;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace WebApplication1.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+
+//        private readonly SiteStatsContext _siteStatsContext;
+
+//        public HomeController(SiteStatsContext statsServiceContext)
+//        {
+//            _siteStatsContext = statsServiceContext;
+//        }
+
+//        public async Task<IActionResult> Index()
+//        {
+//            var stats = await _siteStatsContext.SiteStatsCollection.SingleOrDefaultAsync();
+
+//            var siteStatsModel = SiteStatsService.GetStats(stats);
+
+//            return View(siteStatsModel);
+
+//        }
+
+//        public IActionResult Catalog()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult Conditions()
+//        {
+//            return View();
+//        }
+//        public IActionResult Rewievs()
+//        {
+//            return View();
+//        }
+//        public IActionResult Contacts()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+//    }
+//}
+
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using WebApplication1.Models;
-using WebApplication1.Data;
-using WebApplication1.Services;
-using Microsoft.EntityFrameworkCore;
 
-namespace WebApplication1.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    [Route("")]  // главная страница
+    public IActionResult Index()
     {
-
-        private readonly SiteStatsContext _siteStatsContext;
-
-        public HomeController(SiteStatsContext statsServiceContext)
-        {
-            _siteStatsContext = statsServiceContext;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var stats = await _siteStatsContext.SiteStatsCollection.SingleOrDefaultAsync();
-
-            var siteStatsModel = SiteStatsService.GetStats(stats);
-
-            return View(siteStatsModel);
-
-        }
-
-        public IActionResult Catalog()
-        {
-            return View();
-        }
-
-        public IActionResult Conditions()
-        {
-            return View();
-        }
-        public IActionResult Rewievs()
-        {
-            return View();
-        }
-        public IActionResult Contacts()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View();
     }
 }
