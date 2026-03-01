3e00c855c3271d396d20991a6f291612eb670263
Demonio <s20.rex9000@gmail.com>
2026-03-01 06:36:32 +0300

    add model and context of CarInfo

---
+++ Data/CarInfoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class CarInfoContext : DbContext
+    {
+        public CarInfoContext(DbContextOptions<CarInfoContext> options) 
+            : base(options) { }
+
+        public DbSet<CarInfo> CarInfoCollection { get; set; }
+    }
+}
---
+++ Models/CarInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("car_info")]
+    public class CarInfo
+    {
+        [Column("id")]
+        public int? id { get; set; }
+
+        [Column("make_model")]
+        public string? MakeModel { get; set; }
+
+        [Column("manufacture_year")]
+        public int ManufactureYear { get; set; }
+
+        [Column("photo_path")]
+        public string? PhotoPath { get; set; }
+
+        [Column("engine_volume")]
+        public float EngineVolume { get; set; }
+
+        [Column("daily_cost")]
+        public int DailyCost { get; set; }
+
+        [Column("car_class")]
+        public char CarClass { get; set; }
+    }
+}
