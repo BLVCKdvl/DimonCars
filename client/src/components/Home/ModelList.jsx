a0dd2c5e3a98f195092e48b703a989553bc78b1e
Dmitry Korzhitsky <s20.rex9000@gmail.com>
2026-03-06 15:58:41 +0300

    added some front + styles

--- client/src/components/Home/ModelList.jsx
+++ client/src/components/Home/ModelList.jsx
@@ -28,15 +28,44 @@ export default function ModelList() {
 
   return (
     <section className="model-list">
+      <h1 className="model-header">Каталог автомобилей</h1>
       <div className="model-list-container">
-        <h1>Каталог автомобилей</h1>
+        <div className="row">
+          <div
+            className="column left"
+            style={{
+              backgroundImage: `url(${models.photoPath})`,
+            }}
+          ></div>
+          <div
+            className="column right"
+            style={{
+              backgroundImage: `url(${models.photoPath})`,
+            }}
+          ></div>
+        </div>
+        <div className="row">
+          <div
+            className="column left"
+            style={{
+              backgroundImage: `url(${models.photoPath})`,
+            }}
+          ></div>
+          <div
+            className="column right"
+            style={{
+              backgroundImage: `url(${models.photoPath})`,
+            }}
+          ></div>
+        </div>
+        {/* <h1>Каталог автомобилей</h1>
         <p>{models.makeModel}</p>
         <p>{models.manufactureYear}</p>
         <p>{models.photoPath}</p>
         <p>{models.engineVolume}</p>
         <p>{models.dailyCost}</p>
         <p>{models.carClass}</p>
-        <img src={models.photoPath} alt="авто" />
+        <img src={models.photoPath} alt="авто" /> */}
       </div>
     </section>
   );
