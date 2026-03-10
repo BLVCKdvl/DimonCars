75a4af605996373d174b6bf14479322028c5d30a
Dmitry Korzhitsky <s20.rex9000@gmail.com>
2026-03-10 12:24:14 +0300

    add advantages container + styles

---
+++ client/src/components/Home/Advantages.jsx
@@ -0,0 +1,22 @@
+export default function Advantages() {
+  return (
+    <section class="advantages">
+      <div class="advantages-block">
+        <div class="images-left">
+          <div class="bg-image"></div>
+        </div>
+        <div class="information-right">
+          <h3>
+            Мы поддерживаем идеальное состояние машин и дезинфицируем их перед
+            каждым выездом
+          </h3>
+          <p>
+            Все автомобили укомплектованы сезонной резиной, аудиосистемой и
+            набором Pit Stop
+          </p>
+        </div>
+      </div>
+      <div class="shifter-image"></div>
+    </section>
+  );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Introduction from "../components/Home/Introduction";
 import ModelList from "../components/Home/ModelList";
+import Advantages from "../components/Home/Advantages";
 
 export default function Home() {
   return (
     <>
       <Introduction />
+      <Advantages />
       <ModelList limit={4} />
     </>
   );
